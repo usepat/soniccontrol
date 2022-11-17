375386832ed9509335bbd493a8de68a025584c59
Ilja Golovanov <ilja.golovanov@usepat.com>
2022-11-17 10:06:27 +0100

    updated __init__.py for handlich filenotfound exceptions

--- src/soniccontrol/__init__.py
+++ src/soniccontrol/__init__.py
@@ -1,19 +1,24 @@
 import pyglet
 import os
 
-lines: list = []
+try:
+    lines: list = []
 
-with open("logs//soniccontrol.log") as file:
-    lines: list = file.readlines()
+    with open("logs//soniccontrol.log") as file:
+        lines: list = file.readlines()
 
-if len(lines) > 10000:
-    os.remove("logs//soniccontrol.log")
-        
-with open("logs//sonicpackage.log") as file:
-    lines: list = file.readlines()
+    if len(lines) > 10000:
+        os.remove("logs//soniccontrol.log")
+            
+    with open("logs//sonicpackage.log") as file:
+        lines: list = file.readlines()
 
-if len(lines) > 10000:
-    os.remove("logs//sonicpackage.log")
+    if len(lines) > 10000:
+        os.remove("logs//sonicpackage.log")
+
+except FileNotFoundError as fe:
+    print("File not found, proceeding...")
+    pass
 
 pyglet.font.add_file("src//soniccontrol//fonts//QTypeOT-CondExtraLight.otf")
 pyglet.font.add_file("src//soniccontrol//fonts//QTypeOT-CondLight.otf")
