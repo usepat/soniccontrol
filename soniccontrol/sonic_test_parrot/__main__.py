ffe0dd2c27cc02f7e4c89537ef6713a5f67c0fc9
David Fucking Wild <david.wild@usepat.com>
2024-05-14 11:02:30 +0200

    added helper functions for setting up the parrot training

--- soniccontrol/sonic_test_parrot/__main__.py
+++ soniccontrol/sonic_test_parrot/__main__.py
@@ -1,60 +1,17 @@
 import asyncio
-import json
-import logging
-import pathlib
+from soniccontrol.sonic_test_parrot.training import *
 
-import soniccontrol.utils.constants as const
-from soniccontrol.sonicpackage.builder import AmpBuilder
-from soniccontrol.sonicpackage.serial_communicator import SerialCommunicator
-from soniccontrol.sonic_test_parrot.parrot import Parrot
 
-def setup_logging() -> None:
-    config_file: pathlib.Path = const.files.LOGGING_CONFIG
-    with config_file.open() as file:
-        config = json.load(file)
-    logging.config.dictConfig(config)
-
-async def simulate_soniccontrol_user_interaction():
-    process = await asyncio.create_subprocess_shell(
-        "/home/david-wild/Documents/sonic-firmware/build/linux/test/cli/cli_simulation_mvp/cli_simulation_mvp",
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.STDOUT
-    )
-    communicator = SerialCommunicator()
-    await communicator.connect(process.stdout, process.stdin)
-
-    builder = AmpBuilder()
-    sonicamp = await builder.build_amp(communicator)
-    await sonicamp.execute_command("!f=", "100")
+async def simulate_soniccontrol_user_interaction(sonicamp: SonicAmp):
+    await sonicamp.set_frequency(100)
     await sonicamp.execute_command("?uipt")
-    await sonicamp.execute_command("!ON")
-    communicator.disconnect()
-    process.terminate()
-    await process.wait()
+    await sonicamp.set_signal_on()
+
 
 async def main():
-    setup_logging()
-
-    await simulate_soniccontrol_user_interaction()
-
-    process = await asyncio.create_subprocess_shell(
-        "/home/david-wild/Documents/sonic-firmware/build/linux/test/cli/cli_simulation_mvp/cli_simulation_mvp",
-        stdin=asyncio.subprocess.PIPE,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.STDOUT
-    )
-    communicator = SerialCommunicator()
-    await communicator.connect(process.stdout, process.stdin)
-
-    # if you do not want to feed the parrot when starting soniccontrol, then just set the logging to a higher level than debug 
-    with open("./logs/parrot_food_logs_for_integration_tesing.log", "r") as file:
-        log_lines = file.readlines()
-
-    parrot = Parrot(communicator, log_lines)
-    parrot.register_parrot_log_handler(logging.getLogger("parrot_feeder"))
-    await parrot.setup_amp()
-    await parrot.run_imitation()
+    process_name = "/home/david-wild/Documents/sonic-firmware/build/linux/test/cli/cli_simulation_mvp/cli_simulation_mvp"
+    await cli_wrapper(process_name, teach_parrot, simulate_soniccontrol_user_interaction)
+    await cli_wrapper(process_name, test_parrot)
 
 
 if __name__ == "__main__":
---
+++ soniccontrol/sonic_test_parrot/training.py
@@ -0,0 +1,83 @@
+import asyncio
+import json
+import logging
+import pathlib
+from typing import Any, Callable, List, Optional, Union
+import typing
+from serial_asyncio import open_serial_connection
+
+from soniccontrol.sonicpackage.sonicamp_ import SonicAmp
+import soniccontrol.utils.constants as const
+from soniccontrol.sonicpackage.builder import AmpBuilder
+from soniccontrol.sonicpackage.serial_communicator import SerialCommunicator
+from soniccontrol.sonic_test_parrot.parrot import Parrot
+
+
+def setup_logging() -> None:
+    config_file: pathlib.Path = const.files.LOGGING_CONFIG
+    with config_file.open() as file:
+        config = json.load(file)
+    logging.config.dictConfig(config)
+
+
+CommandList = List[List[str]]
+CommandCaller = Callable[[SonicAmp], typing.Coroutine]
+async def teach_parrot(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, commands: Union[CommandList, CommandCaller]):
+    setup_logging()
+
+    communicator = SerialCommunicator()
+    await communicator.connect(reader,writer)
+
+    builder = AmpBuilder()
+    sonicamp = await builder.build_amp(communicator)
+    
+    if isinstance(commands, list):
+        for command in commands:
+            await sonicamp.execute_command(*command)
+    elif callable(commands):
+        await commands(sonicamp)
+
+    communicator.disconnect()
+
+
+async def test_parrot(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, parrot_food_file: Optional[str] = None):
+    setup_logging()
+
+    communicator = SerialCommunicator()
+    await communicator.connect(reader,writer)
+
+    parrot_feeder = logging.getLogger("parrot_feeder")
+    if parrot_food_file is None:
+        parrot_food_file = parrot_feeder.handlers[0].baseFilename
+    # if you do not want to feed the parrot when starting soniccontrol, then just set the logging to a higher level than debug 
+    with open(parrot_food_file, "r") as file:
+        log_lines = file.readlines()
+
+    parrot = Parrot(communicator, log_lines)
+    parrot.register_parrot_log_handler(parrot_feeder)
+    await parrot.setup_amp()
+    await parrot.run_imitation()
+
+    communicator.disconnect()
+
+
+async def uart_wrapper(port, baudrate, func, *args, **kwargs):
+    reader, writer = await open_serial_connection(url=port, baudrate=baudrate)
+    return await func(reader, writer, *args, **kwargs)
+
+
+async def cli_wrapper(process_name, func, *args, **kwargs):
+    process = await asyncio.create_subprocess_shell(
+        process_name,
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.STDOUT
+    )
+
+    result = await func(process.stdout, process.stdin, *args, **kwargs)
+
+    process.terminte()
+    await process.wait()
+
+    return result
+    
