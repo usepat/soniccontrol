6fab490c12b26a8245022971caace9e6b8e4f810
David Wild <a12134916@unet.univie.ac.at>
2024-08-13 10:25:56 +0200

    Comments in exe script resulted in an error

--- scripts/create_exe.bat
+++ scripts/create_exe.bat
@@ -6,8 +6,5 @@ echo "create exe file with pyinstaller"
 pyinstaller --noconfirm --onedir --windowed ^
 --name "SonicControl" ^
 --contents-directory "." ^
-@REM --add-data "%WORKSPACE_DIR%\resources;resources" ^
-@REM --add-data "%WORKSPACE_DIR%\bin;bin" ^
-@REM --add-data "%WORKSPACE_DIR%\configs;configs" ^
 --collect-all soniccontrol ^
 "%WORKSPACE_DIR%\src\soniccontrol_gui\__main__.py"
